c7cdfde13d2f7ed99852b5bae62832f3c581e337
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 14:46:33 -0500

    rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_basic-nested-forms-lab_session'
+Rails.application.config.session_store :cookie_store, key: '_basic_nested_forms_lab_session'

3fa3acc6baf419341334e03a0554041e6e3fbf08
Andrew Davisson <akdavisson4@gmail.com>
2019-08-15 11:51:16 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  #accepts_nested_attributes_for :categories
 
 
   def categories_attributes=(category_attributes)

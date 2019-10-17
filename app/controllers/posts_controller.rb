1b808eaad23b8a65724fa1a564bfa4722ada0d20
ADIL NAIMI <adilnm23@hotmail.com>
2019-10-17 03:58:37 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    raise params.inspect
-  
+    binding.pry
+
     post = Post.create(post_params)
     redirect_to post
   end

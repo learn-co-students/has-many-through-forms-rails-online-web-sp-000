eddf9d6a03d11051de8db9e0bf4167c6ec0e15b3
wyao905 <warrenyca905@gmail.com>
2019-11-28 15:08:44 -0500

    Done.

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,4 +1,12 @@
 class PostCategory < ActiveRecord::Base
   belongs_to :post
   belongs_to :category
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end

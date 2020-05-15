6ad435f7a2b8f28754eaeba1a94b473a2ec661ea
AnthonynTilelli <anthony@tilelli.me>
2020-05-14 20:50:32 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160115202458) do
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -35,5 +35,4 @@ ActiveRecord::Schema.define(version: 20160115202458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end

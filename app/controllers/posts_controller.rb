62f9001a83038f4f7680c76b431e36c2c02b40b7
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 03:23:53 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,11 @@ class PostsController < ApplicationController
 
   private
 
+#nested params hash with data about the primary object
+#association attributes
+#key of :category_ids with a value of an array
+#sql fires from creating our new post
+#expect key of name inside for the category's name 
   def post_params
  params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,13 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   #accepts_nested_attributes_for :categories
+#bi directional has many through
 
 
+#customize the way our category is created by making this method
+#creating a new category if it doesn't already exist with the current name
+#call self.categories returns an array of category objects
+# then shovel << method to add our newly found or created Category object to the array
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
@@ -11,3 +16,6 @@ class Post < ActiveRecord::Base
     end
   end
 end
+
+
+#customizing active record a little bit 

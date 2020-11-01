5e17fb3683141807e69fd57eb373b7a8963cc662
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-01 04:02:29 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
 
-
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)

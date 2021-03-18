a4015d7b58b1d120d33cee8050ccdfdb716c69c4
Keith Filkins <kafilkins@me.com>
2021-03-18 17:56:59 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,9 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
-  private
+    private
 
-  def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
-  end
+    def post_params
+      params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    end
 end

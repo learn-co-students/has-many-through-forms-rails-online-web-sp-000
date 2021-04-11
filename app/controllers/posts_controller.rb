65ff6ca6c22959cf2178325759c495a2aa5c17df
dbrandt1990 <dvbrandt90@gmail.com>
2021-04-11 16:45:54 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories
+
   def show
     @post = Post.find(params[:id])
   end
@@ -14,6 +18,13 @@ class PostsController < ApplicationController
   def create
     post = Post.create(post_params)
     redirect_to post
+  end
+
+   
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
   end
 
   private

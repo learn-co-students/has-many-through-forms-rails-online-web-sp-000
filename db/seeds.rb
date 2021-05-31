6be0d902693882627193dda185174c0ee2611926
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-31 12:06:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = %w[Fashion
+Food 
+Travel
+Music
+Lifestyle
+Fitness
+DIY
+Sports]
+categories.each do |cat|
+  Category.create({name: "#{cat}"})
+end
